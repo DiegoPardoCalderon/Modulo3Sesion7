1646c515eafeaf74064d11ab66710fdbd5ec5be1
Diego Mauricio Pardo Calderón <128651145+DiegoPardoCalderon@users.noreply.github.com>
2023-05-02 22:12:46 -0400

    ejemplos trabajado en clases

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+//console.log("Funciones Cadena de Caracteres")
+//var nombreApellido = prompt("Ingrese su Nombre y Apellidos")
+
+//ReplaceAll
+//console.log(nombreApellido.replaceAll(" ",""))
+//console.log(nombreApellido.replaceAll("o","1"))
+//console.log(nombreApellido.replaceAll("Carlos", "Luis"))
+
+//Replace
+//console.log(nombreApellido.replace("Carlos", "Luis"))
+
+//Trim
+//console.log(nombreApellido.trim())
+
+//TrimStart
+//console.log(nombreApellido.trimStart())
+
+//TrimEnd
+//console.log(nombreApellido.trimEnd())
+
+var numero = prompt("ingrese un numero...");
+
+var numero2 = 1_500_000
+console.log(numero2);
+
+if(numero >1_000_000){
+    alert("El numero es mayor a 1 millon.");
+    
+}else{
+    alert("El numero es menor o igual a 1 millon");
+}
