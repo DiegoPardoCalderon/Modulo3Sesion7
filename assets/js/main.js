5c6d6b524540c74ad0aa12a69693d09711fe8a02
Diego Mauricio Pardo Calderón <128651145+DiegoPardoCalderon@users.noreply.github.com>
2023-05-03 22:27:09 -0400

    seguimos practicando

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,14 +18,57 @@
 //TrimEnd
 //console.log(nombreApellido.trimEnd())
 
-var numero = prompt("ingrese un numero...");
+//var numero = prompt("ingrese un numero...");
 
-var numero2 = 1_500_000
-console.log(numero2);
+//Ejemplo de separador numerico
+//var numero2 = 1_500_000
+//console.log(numero2);
 
-if(numero >1_000_000){
-    alert("El numero es mayor a 1 millon.");
+//Ejemplo de condiciones con separador numerico
+//if(numero >1_000_000){
+   // alert("El numero es mayor a 1 millon.");
     
-}else{
-    alert("El numero es menor o igual a 1 millon");
-}
+//}else{
+  //  alert("El numero es menor o igual a 1 millon");
+//}
+
+//Operador de fusion nula
+var colaboradores = [
+    { 
+        rut: '12345678-9', 
+        nombre: "Maria Morales", 
+        departamento: "Talento Humano", 
+        jornada: "completa",
+        fecha_ingreso: "2021-01-16",
+        direccion: {
+            region: 'Bio-Bio',
+            comuna: 'Hualpen',
+        },
+        salario:{
+            salario_bruto:1_800_000,
+            retenciones: {
+                AFP: 1_200,
+                SALUD: 1_500,
+        },
+        salario_neto: 1_797_300
+    }, 
+    },   
+    { rut: '22334455-3', nombre: "Jose Medina", departamento: "Informatica", jornada:"Medio Tiempo"},
+    { rut: '33421456-0', nombre: "Carlos Lopez"},//caso undefined
+    { rut: '55667788-1', nombre: "Pedro Molina", departamento: "", jornada:"Completa"},//caso cadena vacia
+    { rut: '54567288-1', nombre: "Juan Pavez", departamento: null }//caso valor null
+]
+// console.log(colaboradores)
+
+colaboradores.map(colaborador => {
+    //var direccion = colaborador?.direccion?.region || ''
+    var retencionAFP =colaborador?.salario?.retenciones?.AFP || 0
+    console.log("AFP", retencionAFP)
+    colaborador.salario = 2_500_000
+    colaborador.departamento = colaborador.departamento ?? "Servicios Generales"
+    colaborador.jornada &&= colaborador.jornada.toUpperCase()
+    colaborador.fecha_ingreso ||= "2023-01-01"
+    return colaborador
+})
+
+// console.log(colaboradores)
